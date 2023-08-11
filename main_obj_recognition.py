670ddc58ba35f22ac9a81c55c697ae9957be078a
Pinyuan Feng <scypf1@nottingham.ac.uk>
2023-08-11 16:02:34 -0400

    debug process 0 terminated with exit code 17

--- main_obj_recognition.py
+++ main_obj_recognition.py
@@ -395,26 +395,26 @@ def main():
         scheduler.step()
 
         # save model for best_acc model
-        if epoch < config.epochs // 2: continue
-        is_best_acc = val_acc > best_acc
-        best_acc = max(val_acc, best_acc)
-        save_checkpoint({
-            'epoch': epoch + 1,
-            "model_name": config.model_name,
-            'state_dict': model.state_dict(),
-            'acc': val_acc,
-            'best_acc': best_acc,
-            'optimizer': optimizer.state_dict(),
-            'scheduler' : scheduler.state_dict(),
-            'mode':config.mode
-        }, is_best_acc)
+        # if epoch < config.epochs // 2: continue
+        # is_best_acc = val_acc > best_acc
+        # best_acc = max(val_acc, best_acc)
+        # save_checkpoint({
+        #     'epoch': epoch + 1,
+        #     "model_name": config.model_name,
+        #     'state_dict': model.state_dict(),
+        #     'acc': val_acc,
+        #     'best_acc': best_acc,
+        #     'optimizer': optimizer.state_dict(),
+        #     'scheduler' : scheduler.state_dict(),
+        #     'mode':config.mode
+        # }, is_best_acc)
         
         gc.collect()
 
 if __name__ == '__main__':
     
     if config.tpu == True:
-        tpu_cores_per_node = 8
+        tpu_cores_per_node = 1
         xmp.spawn(main(), args=(), nprocs=tpu_cores_per_node) # cannot call xm.xla_device() before spawing
     else:
         main()
