60c1d8cd63629867a0561bcc740f4b30a6fb5df0
Pinyuan Feng <scypf1@nottingham.ac.uk>
2023-08-24 16:46:46 -0400

    check avgmeter

--- utils.py
+++ utils.py
@@ -53,14 +53,12 @@ class AverageMeter(object):
         self.avg = 0
         self.sum = 0
         self.count = 0
-        return
 
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-        return
 
     def __str__(self):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
